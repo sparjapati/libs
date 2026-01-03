d18742e5c97c3f8b0cd460c41f2d02fab1dc88e0
Sanjay Verma <sanjay.verma@ofbusiness.in>
2026-01-03 09:17:36 +0530

    made centralized versioning

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,12 @@ plugins {
     kotlin("plugin.jpa") version "1.9.22" apply false
 }
 subprojects {
-
-    group = "com.sparjapati"
-    version = "0.0.1"
+    group = property("group") as String
 
     repositories {
         mavenCentral()
     }
+}
+project(":dbstore") {
+    version = property("dbstoreVersion") as String
 }
--- dbstore/build.gradle.kts
+++ dbstore/build.gradle.kts
@@ -6,26 +6,20 @@ plugins {
     `java-library`
 }
 dependencies {
-    api(platform("org.springframework:spring-framework-bom:6.1.4"))
-    api(platform("org.springframework.data:spring-data-bom:2023.1.2"))
-    api("org.springframework:spring-context")
-    api("org.springframework:spring-aop")
-    api("org.springframework:spring-tx")
-    api("jakarta.persistence:jakarta.persistence-api:3.1.0")
-    api("org.springframework.data:spring-data-jpa")
-    api("org.aspectj:aspectjweaver:1.9.22.1")
-
-    implementation("org.slf4j:slf4j-api:2.0.12")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
-    implementation("org.slf4j:slf4j-api:2.0.12")
-}
+    api(platform(libs.spring.framework.bom))
+    api(platform(libs.spring.data.bom))
+    api(libs.bundles.spring.core)
+    api(libs.spring.data.jpa)
+    api(libs.jakarta.persistence)
+    api(libs.aspectj.weaver)
+    implementation(libs.jackson.databind)
+    implementation(libs.slf4j.api)
+}9053204221
 publishing {
     publications {
         create<MavenPublication>("mavenJava") {
             from(components["java"])
-            groupId = "com.sparjapati"
             artifactId = "dbstore"
-            version = "0.0.1"
         }
     }
 }
